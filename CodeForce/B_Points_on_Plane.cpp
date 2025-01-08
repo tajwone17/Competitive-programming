b1439621ac45e2c904991db0da64170f994b9227
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-01-09 00:45:28 +0600

    add solutions for Codeforces problems B. Points on Plane and C1. Powering the Hero (easy version); include problem metadata and initial implementations

---
+++ CodeForce/B_Points_on_Plane.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    // cin.ignore();
+    while (t--)
+    {
+        int n, result;
+        cin >> n;
+        if (n == 1)
+            result = 0;
+        else
+            result = sqrt(n - 1);
+        cout << result << endl;
+    }
+}
