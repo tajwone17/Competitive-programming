cb8ece985bbf8c8bc92b8170080bd913811aa45e
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-01-28 21:27:32 +0600

    add solution for Codeforces problem C. Palindromic Subsequences; include implementation and problem metadata

---
+++ CodeForce/C_Palindromic_Subsequences.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// // long long mod(long long x){
+// //     return ((x%M + M)%M);
+// // }
+// // long long add(long long a, long long b){
+// //     return mod(mod(a)+mod(b));
+// // }
+// long long mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+
+main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    // cin.ignore();
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == 1 || i == 2 || i == n)
+                cout << 1 << " ";
+            else
+                cout << i << " ";
+        }
+        cout << endl;
+    }
+}
