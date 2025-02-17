8f49bebe2feceda1a1b4416e29229e9c8efd5993
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-02-17 23:35:18 +0600

    add solutions for Codeforces problems A. Brogramming Contest and A. Pangram; include implementations and problem metadata

---
+++ CodeForce/A_Brogramming_Contest.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define all(n) for (int i = 0; i < (n); i++)
+#define sort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).rbegin(), (v).rend())
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// // long long mod(long long x){
+// //     return ((x%M + M)%M);
+// // }
+// // long long add(long long a, long long b){
+// //     return mod(mod(a)+mod(b));
+// // }
+// long long mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+void solve()
+{
+    int n, ans = 0;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (s[i] == '1' && s[i + 1] == '0' || s[i] == '0' && s[i + 1] == '1')
+            ans++;
+    }
+    if (s[0] == '1')
+        cout << ans + 1 << endl;
+    else
+        cout << ans << endl;
+}
+
+main()
+{
+    faster;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        solve();
+    }
+}
