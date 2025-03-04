a1d8c3a4e394d86ceacf01f25d8ae22eef81015f
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-03-04 23:14:19 +0600

    add solution for Codeforces problem A. Gravity Flip; include implementation

---
+++ CodeForce/A_Gravity_Flip.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define faster                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define all(n) for (int i = 0; i < (n); i++)
+#define sort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).rbegin(), (v).rend())
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// // long long mod(long long x){
+// //     return ((x%M + M)%M);
+// // }
+// // long long add(long long a, long long b){
+// //     return mod(mod(a)+mod(b));
+// // }
+// long long mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v);
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+}
+
+main()
+{
+    faster;
+
+    solve();
+}
