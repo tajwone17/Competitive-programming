26ad5860b6908d822043ed5859983a0a74bdfc3b
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-04-16 22:28:50 +0600

    add solution for Codeforces problem B. Two Buttons; include full implementation

---
+++ CodeForce/B_Two_Buttons.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define all(n) for (int i = 0; i < (n); i++)
+#define sort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).rbegin(), (v).rend())
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// int mod(long long x){
+//     return ((x%M + M)%M);
+// }
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+// int mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    int a,b,ans=0;
+    cin>>a>>b;
+    while(b>a){
+        if(b%2==0){
+            b/=2;
+           
+        }
+        else{
+            b++;
+        }
+        ans++;
+    }
+    cout<<ans+a-b<<endl;
+    
+}
+
+main()
+{
+    freePalestine;
+  
+        tajwone17();
+    
+}
