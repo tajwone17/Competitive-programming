9caa3ede4a5257339759cbd0888737ab3d45e328
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-05-02 02:08:21 +0600

    new solutions added

---
+++ C_Neo_s_Escape.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// int mod(long long x){
+//     return ((x%M + M)%M);
+// }
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+// int mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; i++)
+    {
+        v.push_back({a[i], i});
+    }
+    sort(v.rbegin(), v.rend());
+    b[v[0].second] = 1;
+    if (v[0].second < n - 1)
+        b[v[0].second + 1] = 1;
+    if (v[0].second > 0)
+        b[v[0].second - 1] = 1;
+    int cnt = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (b[v[i].second] == 1)
+        {
+            if (v[i].second < n - 1)
+                b[v[i].second + 1] = 1;
+            if (v[i].second > 0)
+                b[v[i].second - 1] = 1;
+        }
+        else
+        {
+            b[v[i].second] = 1;
+            if (v[i].second < n - 1)
+                b[v[i].second + 1] = 1;
+            if (v[i].second > 0)
+                b[v[i].second - 1] = 1;
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+}
+
+int32_t main()
+{
+    freePalestine;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        tajwone17();
+    }
+}
---
+++ CodeForce/A_Johnny_and_Ancient_Computer.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define all(n) for (int i = 0; i < (n); i++)
+#define sort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).rbegin(), (v).rend())
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// int mod(long long x){
+//     return ((x%M + M)%M);
+// }
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+// int mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    int a, b;
+    cin >> a >> b;
+    int ta = a, tb = b;
+    while (ta % 2 == 0)
+        ta /= 2;
+    while (tb % 2 == 0)
+        tb /= 2;
+    if (ta != tb)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    a /= ta;
+    b /= tb;
+    int x = log2(a);
+    int y = log2(b);
+    int ans = (abs(x - y) + 2) / 3;
+    cout << ans << endl;
+}
+int32_t main()
+{
+    freePalestine;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        tajwone17();
+    }
+}
