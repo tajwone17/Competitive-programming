30f2b79d32c42a6bca6f895a343dccca9317b307
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-05-04 00:29:59 +0600

    red versus blue problem solved

---
+++ CodeForce/B_Beautiful_Array.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define all(n) for (int i = 0; i < (n); i++)
+#define sort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).rbegin(), (v).rend())
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// int mod(long long x){
+//     return ((x%M + M)%M);
+// }
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+// int mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    int n, k, b, s;
+    cin >> n >> k >> b >> s;
+    vector<int> ans(n, 0);
+    int z = (k * b) + k - 1;
+    if ((((k - 1) * (n - 1)) + z) < s)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    if (k * b > s)
+    {
+        cout << -1 << endl;
+        return;
+    }
+
+    if (s < z)
+    {
+        ans[0] = k * b + (s - (k * b));
+    }
+    else
+    {
+        ans[0] = z;
+        for (int i = 1; i < n; i++)
+        {
+            if (z == s)
+                break;
+            if ((s - z) < k - 1)
+            {
+                ans[i] = s - z;
+                break;
+            }
+
+            ans[i] = k - 1;
+            z = z + k - 1;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+int32_t main()
+{
+    freePalestine;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        tajwone17();
+    }
+}
