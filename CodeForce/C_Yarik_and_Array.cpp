7dfa5d4f3fc42f7095910d4e1bebff240770bfd5
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-05-15 01:05:47 +0600

    add solutions for B_Deja_Vu and C_Yarik_and_Array problems

---
+++ CodeForce/C_Yarik_and_Array.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define all(n) for (int i = 0; i < (n); i++)
+#define sort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).rbegin(), (v).rend())
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// int mod(long long x){
+//     return ((x%M + M)%M);
+// }
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+// int mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    all(n) cin >> a[i];
+
+    if (n == 1)
+    {
+        cout << a[0] << endl;
+        return;
+    }
+
+    int ans = a[0];
+    int sum = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (sum < 0)
+        {
+            sum = 0;
+            sum = a[i];
+        }
+        else if ((a[i] & 1) != (a[i - 1] & 1))
+        {
+            // sum = max(a[i], sum + a[i]);
+            sum += a[i];
+        }
+        else
+        {
+            sum = a[i];
+        }
+
+        ans = max(ans, sum);
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    freePalestine;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        tajwone17();
+    }
+}
