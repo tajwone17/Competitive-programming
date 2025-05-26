65a6eff1afd56ba1284b26c1d79deaae3dc775b7
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-05-27 02:15:01 +0600

    add solutions for A_Square_Year, B_A_Perfectly_Balanced_String, and F_Eating_Candies problems

---
+++ CodeForce/B_A_Perfectly_Balanced_String.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// int mod(long long x){A
+//     return ((x%M + M)%M);
+// }
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+// int mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    string s;
+
+    cin >> s;
+    set<char> st;
+    int k;
+    for (k = 0; k < s.size(); k++)
+    {
+        if (st.find(s[k]) == st.end())
+        {
+            st.insert(s[k]);
+        }
+        else
+        {
+            break;
+        }
+    }
+    for (int i = k; i < s.size(); i++)
+    {
+        if (s[i] != s[i - k])
+        {
+            no;
+            return;
+        }
+    }
+    yes;
+}
+int32_t main()
+{
+    freePalestine;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        tajwone17();
+    }
+}
