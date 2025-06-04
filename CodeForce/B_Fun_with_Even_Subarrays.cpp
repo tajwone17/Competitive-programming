02a5199d5d66c0574f84e1c0a0aaf1f22531902a
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2025-06-04 23:09:26 +0600

    add solution for B_Kill_Demodogs and C_Paint_the_Array problems

---
+++ CodeForce/B_Fun_with_Even_Subarrays.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+// const int M=998244353;
+// int mod(long long x){A
+//     return ((x%M + M)%M);
+// }
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+// int mul(long long a, long long b){
+//     return mod(mod(a)*mod(b));
+// }
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int ans = 0;
+    int i = n - 1;
+    while (i >= 0 && a[i] == a[n - 1])
+    {
+        i--;
+    }
+    if (i == -1)
+    {
+        cout << 0 << endl;
+        return;
+    }
+
+    while (i >= 0)
+    {
+        i -= n - i - 1;
+        ans++;
+        while (i >= 0 && a[i] == a[n - 1])
+        {
+            i--;
+        }
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    freePalestine;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        tajwone17();
+    }
+}
---
+++ CodeForce/B_Kill_Demodogs.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define freePalestine             \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long int
+#define endl "\n"
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+// const int N = 1e5 + 10;
+// const int MAX = 200'007;
+// int a[MAX];
+
+const int M = 1e9 + 7;
+int mod(long long x)
+{
+    return ((x % M + M) % M);
+}
+// int add(long long a, long long b){
+//     return mod(mod(a)+mod(b));
+// }
+int mul(long long a, long long b)
+{
+    return mod(mod(a) * mod(b));
+}
+// int mod_pow(int base, int exp) {
+//     int res = 1;
+//     while (exp > 0) {
+//         if (exp & 1)
+//             res = (res * base) % M;
+//         base = (base * base) % M;
+//         exp >>= 1;
+//     }
+//     return res;
+// }
+void tajwone17()
+{
+    int n;
+    cin >> n;
+    int ans = (mul(mul((n * (n + 1)), ((4 * n - 1))), 337));
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    freePalestine;
+    int t;
+    cin >> t;
+    // cin.ignore();
+    for (int i = 1; i <= t; i++)
+    {
+        // cout<<"Case #"<<i<<": ";
+        tajwone17();
+    }
+}
